8e92d8d81f2b798d881616bb9cd1f8ba97d58547
Renato Zangerolami <renato.zanger@gmail.com>
2025-10-30 13:06:28 -0300

    feat: inclui novos outputs

--- output.tf
+++ output.tf
@@ -6,14 +6,18 @@
 #   value = module.eks_cluster.oidc
 # }
 #
-# output "ca" {
-#   value = module.eks_cluster.certicate_authority
-# }
-#
-# output "cluster_endpoint" {
-#   value = module.eks_cluster.cluster_endpoint
-# }
+output "certicate_authority" {
+  value = module.eks_cluster.certicate_authority
+}
+
+output "cluster_endpoint" {
+  value = module.eks_cluster.cluster_endpoint
+}
 
 output "dns_load_balancer_hostname" {
   value = module.k8s_app.alb_dns_name
+}
+
+output "cluster_name" {
+  value = module.eks_cluster.cluster_name
 }
