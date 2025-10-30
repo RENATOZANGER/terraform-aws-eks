17358bf02984ec1ce879984d0c9cff409a9e4646
Renato Zangerolami <renato.zanger@gmail.com>
2025-10-30 17:02:32 -0300

    fix: inclui depends on nat gateway

--- modules/network/ngw.tf
+++ modules/network/ngw.tf
@@ -19,6 +19,7 @@ resource "aws_eip" "eks_ngw_eip_1b" {
 }
 
 resource "aws_nat_gateway" "eks_ngw_1a" {
+  depends_on = [aws_internet_gateway.eks_igw]
   allocation_id = aws_eip.eks_ngw_eip_1a.id
   subnet_id     = aws_subnet.eks_subnet_public_1a.id
   tags = merge(
@@ -30,6 +31,7 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
 }
 
 resource "aws_nat_gateway" "eks_ngw_1b" {
+  depends_on = [aws_internet_gateway.eks_igw]
   allocation_id = aws_eip.eks_ngw_eip_1b.id
   subnet_id     = aws_subnet.eks_subnet_public_1b.id
   tags = merge(
