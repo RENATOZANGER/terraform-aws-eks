31d12f9c511135274f338225d47a099ab229ba70
Renato Zangerolami <renato.zanger@gmail.com>
2025-10-30 19:08:14 -0300

    refactor: remove output alb dns

--- modules/k8s-app/output.tf
+++ modules/k8s-app/output.tf
@@ -1,8 +1,3 @@
-output "alb_dns_name" {
-  description = "ALB's public DNS created by Ingress"
-  value       = kubernetes_ingress_v1.app_ingress.status[0].load_balancer[0].ingress[0].hostname
-}
-
 output "kubernetes_ingress_v1_id" {
   value = kubernetes_ingress_v1.app_ingress.id
 }
