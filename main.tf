1e6c2ef7083ec4da77bb23fe81784a526fb6d98b
Renato Zangerolami <renato.zanger@gmail.com>
2025-10-30 14:57:04 -0300

    refactor: refatora provider

--- main.tf
+++ main.tf
@@ -34,3 +34,26 @@ module "eks_load_balancer_controller" {
 module "k8s_app" {
   source = "./modules/k8s-app"
 }
+
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certicate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certicate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    }
+  }
+}
--- provider.tf
+++ provider.tf
@@ -24,25 +24,3 @@
 #   region  = "us-east-1"
 #   profile = "renato_admin"
 # }
-#
-# provider "kubernetes" {
-#   host                   = module.eks_cluster.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks_cluster.certicate_authority)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-#   }
-# }
-#
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks_cluster.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks_cluster.certicate_authority)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-#     }
-#   }
-# }
